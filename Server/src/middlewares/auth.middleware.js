16a69c2198bb2c943ae2e0deb8f0a0f2a62df70b
adarsh311002 <adarsh.hit.aeie25@heritageit.edu.in>
2025-11-18 00:13:38 +0530

    auth middleware

---
+++ Server/src/middlewares/auth.middleware.js
@@ -0,0 +1,38 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.models.js";
+
+const jwtVerify = async (req, res, next) => {
+  const authHeader = req.header("Authorization");
+  const bearer =
+    authHeader && authHeader.startsWith("Bearer ")
+      ? authHeader.replace("Bearer ", "")
+      : null;
+
+  const token = bearer || req.cookies?.accessToken;
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized: token required" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const user = await User.findById(decoded?._id).select(
+      "-password -refreshToken"
+    );
+
+    if (!user) {
+      return res.status(401).json({ message: "Unauthorised!" });
+    }
+
+    req.user = { id: user._id, email: user.email, role: user.role };
+    next();
+  } catch (error) {
+    console.error("JWT verify error:", err.message);
+    return res.status(401).json({
+      message: "Invalid or Expired Access Token",
+    });
+  }
+};
+
+
+export {jwtVerify}
