042c50727d1f2d8a3bd1101590c02c998d732bf1
adarsh311002 <adarsh.hit.aeie25@heritageit.edu.in>
2025-11-17 22:01:30 +0530

    refreshToken route + controller

--- Server/src/controllers/user.controller.js
+++ Server/src/controllers/user.controller.js
@@ -154,4 +154,45 @@ const userLogin = async (req, res) => {
   }
 };
 
-export { userSignup, userLogin };
+const refreshToken = async (req, res) => {
+  try {
+    const token = req.cookies?.refreshToken;
+
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Refresh Token required",
+      });
+    }
+
+    const decoded = jwt.verify(token, process.env.REFRESH_TOKEN_SECRET);
+    const user = await User.findOne({
+      _id: decoded._id,
+      refreshToken: token,
+    }).select("+refreshToken");
+
+    if (!user) {
+      res.clearCookie("refreshToken");
+      return res.status(401).json({
+        success: false,
+        message: "Invalid refresh token",
+      });
+    }
+
+    const accessToken = user.generateAccessToken();
+
+    res.status(200).json({
+      success: true,
+      accessToken,
+    });
+  } catch (error) {
+    res.clearCookie("refreshToken");
+    return res.status(401).json({
+      success: false,
+      message: "Invalid refresh token",
+      error: error.message,
+    });
+  }
+};
+
+export { userSignup, userLogin, refreshToken };
--- Server/src/routes/user.routes.js
+++ Server/src/routes/user.routes.js
@@ -1,11 +1,12 @@
 import {Router} from "express"
-import { userSignup,userLogin } from "../controllers/user.controller.js"
+import { userSignup,userLogin,refreshToken } from "../controllers/user.controller.js"
 
 
 const router = Router();
 
 router.route("/signup").post(userSignup)
 router.route("/login").post(userLogin)
+router.route("/refresh").post(refreshToken)
 
 
 export default router;
