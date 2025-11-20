6da5448354731672a981852667062869f4677e91
adarsh311002 <adarsh.hit.aeie25@heritageit.edu.in>
2025-11-20 18:58:48 +0530

    adding interceptors

--- Client/src/utils/api.js
+++ Client/src/utils/api.js
@@ -1,7 +1,65 @@
-import axios from "axios"
+import axios from "axios";
+
 
 export const api = axios.create({
   baseURL: "http://localhost:8001/api/v1/",
   withCredentials: true,
 });
 
+
+//request interceptors
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+//response interceptors(is like middeware) for handling token expiries
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+        originalRequest._retry = true;
+
+     try {
+       const currentRefreshToken = localStorage.getItem("refreshToken");
+
+       if (!currentRefreshToken) {
+         throw new Error("No refresh token available");
+       }
+
+       const response = await axios.post(
+         "http://localhost:8001/api/v1/users/refresh",
+         { refreshToken: currentRefreshToken },
+         { withCredentials: true }
+       );
+
+       const { accessToken } =  response.data;
+       localStorage.setItem("accessToken", accessToken);
+
+       api.defaults.headers.common[
+         "Authorization"
+       ] = `Bearer ${accessToken}`;
+       originalRequest.headers["Authorization"] = `Bearer ${accessToken}`;
+
+       return api(originalRequest);
+     } catch (refreshError) {
+       console.log("Session expired. Please login again.");
+       localStorage.clear();
+       window.location.href = "/login";
+     }
+    }
+
+    return Promise.reject(error);
+  }
+);
