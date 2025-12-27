9ba916e62daf4ea3e2e4d00d26880077fa1da704
adarsh311002 <adarsh.hit.aeie25@heritageit.edu.in>
2025-12-27 19:11:39 +0530

    redis client connection

---
+++ Server/src/db/redisClient.js
@@ -0,0 +1,14 @@
+import Redis from "ioredis";
+
+const redis = new Redis(process.env.REDIS_URI || "redis://localhost:6379");
+
+redis.on("connect", () => {
+    console.log("Connected to Redis successfully!");
+});
+
+redis.on("error",(err) => {
+    console.error("Redis connection error:", err);
+});
+
+export default redis;
+
